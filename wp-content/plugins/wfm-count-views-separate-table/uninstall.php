34eaa80e9695219f7a817e652b17aa0c5b119224
tarasdrobotko <drobotkot@gmail.com>
2021-05-10 22:45:31 +0300

    .gitignore fix

--- wp-content/plugins/wfm-count-views-separate-table/uninstall.php
+++
@@ -1,8 +0,0 @@
-<?php 
-if(!defined('WP_UNINSTALL_PLUGIN')) exit;
-
-global $wpdb;
-
-
-$query = "DROP TABLE IF EXISTS wfm_views";
-	$wpdb->query($query);
--- wp-content/plugins/wfm-widget-cats/js/wfm-scripts.js
+++
@@ -1,9 +0,0 @@
-//http://www.designchemical.com/lab/jquery-vertical-accordion-menu-plugin/getting-started/
-jQuery(document).ready(function( $ ) {
-    $('ul.accordion').dcAccordion({
-    	hoverDelay: wfm_obj.hoverDelay,
-    	eventType: wfm_obj.eventType,
-      disableLink: true,
-      speed: wfm_obj.speed
-    });
-});
